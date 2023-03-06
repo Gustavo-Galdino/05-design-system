208c34b17d9c721a704d6e93452b4e91b60de598
Gustavo Galdino <gustavogaldinomartins@gmail.com>
2023-03-06 10:53:51 -0300

    fix: Add ref to TextInput and adjust styles

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -35,5 +35,6 @@ export const Disabled: StoryObj<TextInputProps> = {
 export const Withprefix: StoryObj<TextInputProps> = {
   args: {
     prefix: 'cal.com/',
+    placeholder: 'your-username',
   },
 }
